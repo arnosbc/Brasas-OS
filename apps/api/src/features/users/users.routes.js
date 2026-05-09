4ddaf27f91b6e602b2558aa154e540b96b0f5015
Jose Gomez <jjuliogomez@unicesar.edu.co>
2026-05-09 00:57:13 -0500

    Modicacion de routes.js

--- apps/api/src/features/users/users.routes.js
+++ apps/api/src/features/users/users.routes.js
@@ -9,8 +9,8 @@ router.use(requireRole('ADMIN'));
 
 router.post('/', usersController.createUser);
 router.get('/', usersController.getUsers);
-// router.get('/:id', usersController.getUserById);
-// router.put('/:id', usersController.updateUser);
-// router.delete('/:id', usersController.deactivateUser); // Soft delete
+router.get('/:id', usersController.getUserById);
+router.put('/:id', usersController.updateUser);
+router.delete('/:id', usersController.deactivateUser); // Soft delete
 
 module.exports = router;
